14507a36d5e8ea36ec97fcfaaaf2c6ecd5c589e7
asimmon3 <asimmon3@nd.edu>
2021-01-31 17:35:55 -0500

    Added the weather to the news

--- scripts/main.js
+++ scripts/main.js
@@ -28,3 +28,23 @@ window.onclick = function(event) {
     }
   }
 }
+
+/* Adding the weather script */
+
+n =  new Date();
+  y = n.getFullYear();
+  m = n.getMonth() + 1;
+  d = n.getDate();
+  document.getElementById("today-date").innerHTML = m + "/" + d + "/" + y;
+n_2 = new Date();
+  setDate(n_2.getDate() + 1);
+  y = n_2.getFullYear();
+  m = n_2.getMonth() + 1;
+  d = n_2.getDate();
+  document.getElementById("tomorrow-date").innerHTML = m + "/" + d + "/" + y;
+n_3 = new Date();
+  setDate(n_3.getDate() + 2);
+  y = n_3.getFullYear();
+  m = n_3.getMonth() + 1;
+  d = n_3.getDate();
+  document.getElementById("two-days-date").innerHTML = m + "/" + d + "/" + y;
